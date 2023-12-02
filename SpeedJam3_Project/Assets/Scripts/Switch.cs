94902334e3293097ee49ae116ae39684d4681f40
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2023-12-01 23:21:09 -0300

    feat: switch added.

---
+++ SpeedJam3_Project/Assets/Scripts/Switch.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Switch : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer _spriteRenderer = null;
+    [SerializeField] bool _isActivated = false;
+    [SerializeField] UnityEvent _onEnabled = null;
+
+    private void OnTriggerEnter2D(Collider2D _other)
+    {
+        if (_isActivated) return;
+
+        if (_other.gameObject.CompareTag("Player"))
+        {
+            _isActivated = true;
+            _spriteRenderer.color = Color.green;
+            _onEnabled?.Invoke();
+        }
+    }
+}
--- SpeedJam3_Project/Assets/Scripts/Traps/LaserBehaviour.cs
+++ SpeedJam3_Project/Assets/Scripts/Traps/LaserBehaviour.cs
@@ -8,15 +8,27 @@ public class LaserBehaviour : MonoBehaviour
     [Title("// General")]
     [SerializeField] LineRenderer _lineRenderer = null;
     [SerializeField] Collider2D _collider = null;
-    [SerializeField] bool _isEnabled = true;
+    //[SerializeField] bool _isEnabled = true;
 
     [Title("// Damage")]
     [SerializeField] float _damage = 99;
 
-    private void Update()
+    //private void Update()
+    //{
+    //    _lineRenderer.enabled = _isEnabled;
+    //    _collider.enabled = _isEnabled;
+    //}
+
+    public void EnableVisuals()
     {
-        _lineRenderer.enabled = _isEnabled;
-        _collider.enabled = _isEnabled;
+        _lineRenderer.enabled = true;
+        _collider.enabled = true;
+    }
+
+    public void DisableVisuals()
+    {
+        _lineRenderer.enabled = false;
+        _collider.enabled = false;
     }
 
     private void OnTriggerEnter2D(Collider2D _other)
