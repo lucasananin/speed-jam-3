56e61a186a2d8f18e3f1ca9ec514bc01e8ca1ae9
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2023-12-02 14:09:05 -0300

    feat: prefabs and testSite scene added.

---
+++ SpeedJam3_Project/Assets/Scripts/Holes/SpiderWeb.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderWeb : MonoBehaviour
+{
+    // usar colisor.
+    // ao entrar, deixa lento.
+    // ao sair, reseta a velocidade.
+}
