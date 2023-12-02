9555a3172039f9dfaf50687076f6e335e4e302a0
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2023-12-02 15:46:55 -0300

    feat: laser malfunctioning behaviour added.

--- SpeedJam3_Project/Assets/Scripts/Traps/LaserBehaviour.cs
+++ SpeedJam3_Project/Assets/Scripts/Traps/LaserBehaviour.cs
@@ -10,17 +10,48 @@ public class LaserBehaviour : MonoBehaviour
     [SerializeField] Collider2D _collider = null;
     //[SerializeField] bool _isEnabled = true;
 
-    //[Title("// States")]
-    //[SerializeField] 
+    [Title("// States")]
+    [SerializeField] bool _isMalfunctioning = false;
+    [SerializeField] bool _isOn = true;
+    [SerializeField] float _timeOn = 2f;
+    [SerializeField] float _timeOff = 4f;
+    [SerializeField, ReadOnly] float _nextState = 0f;
 
     [Title("// Damage")]
     [SerializeField] float _damage = 99;
 
-    //private void Update()
-    //{
-    //    _lineRenderer.enabled = _isEnabled;
-    //    _collider.enabled = _isEnabled;
-    //}
+    private void Awake()
+    {
+        _nextState = 0f;
+    }
+
+    private void Update()
+    {
+        if (!_isMalfunctioning) return;
+
+        _nextState += Time.deltaTime;
+
+        if (_isOn && _nextState > _timeOn)
+        {
+            _nextState = 0;
+            _isOn = !_isOn;
+            DisableVisuals();
+        }
+        else if (!_isOn && _nextState > _timeOff)
+        {
+            _nextState = 0;
+            _isOn = !_isOn;
+            EnableVisuals();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D _other)
+    {
+        if (_other.gameObject.CompareTag("Player"))
+        {
+            _other.GetComponent<HealthSystem>().TakeDamage(_damage);
+        }
+    }
 
     public void EnableVisuals()
     {
@@ -32,13 +63,5 @@ public class LaserBehaviour : MonoBehaviour
     {
         _lineRenderer.enabled = false;
         _collider.enabled = false;
-    }
-
-    private void OnTriggerEnter2D(Collider2D _other)
-    {
-        if (_other.gameObject.CompareTag("Player"))
-        {
-            _other.GetComponent<HealthSystem>().TakeDamage(_damage);
-        }
     }
 }
