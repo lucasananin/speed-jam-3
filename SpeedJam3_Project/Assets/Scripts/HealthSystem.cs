b9e56f20515bc8c4602ed45454a6d8925e13012c
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2023-12-03 21:50:48 -0300

    feat: damage color change vfx.

--- SpeedJam3_Project/Assets/Scripts/HealthSystem.cs
+++ SpeedJam3_Project/Assets/Scripts/HealthSystem.cs
@@ -3,12 +3,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class HealthSystem : MonoBehaviour
 {
     [SerializeField] bool _isInvincible = false;
     [SerializeField] float _maxHealth = 3;
     [SerializeField, ReadOnly] float _currentHealth = 0;
+
+    [Title("// Vfx")]
+    [SerializeField] List<SpriteRenderer> _spriteRenderers = null;
+    [SerializeField] float _duration = 0.1f;
 
     public event Action onDamageTaken = null;
     public event Action onDead = null;
@@ -39,7 +44,22 @@ public class HealthSystem : MonoBehaviour
         }
         else
         {
+            PlayTakeDamageColorVfx();
             onDamageTaken?.Invoke();
+        }
+    }
+
+    private void PlayTakeDamageColorVfx()
+    {
+        if (_spriteRenderers.Count <= 0) return;
+
+        int _count = _spriteRenderers.Count;
+
+        for (int i = 0; i < _count; i++)
+        {
+            _spriteRenderers[i].DOComplete();
+            _spriteRenderers[i].color = Color.red;
+            _spriteRenderers[i].DOColor(Color.white, _duration);
         }
     }
 
