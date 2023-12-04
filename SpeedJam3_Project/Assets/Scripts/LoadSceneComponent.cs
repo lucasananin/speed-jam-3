49a588847cda052e4546b4a413fb295e88e89261
Lucas Bernard <lucasfagundes.gweb@gmail.com>
2023-12-04 17:09:53 -0300

    ghgjshs

--- SpeedJam3_Project/Assets/Scripts/LoadSceneComponent.cs
+++ SpeedJam3_Project/Assets/Scripts/LoadSceneComponent.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,7 @@ public class LoadSceneComponent : MonoBehaviour
 {
     [SerializeField] string _sceneName = null;
 
+    [Button]
     public void LoadScene()
     {
         if (LoadSceneManager.Instance == null) return;
